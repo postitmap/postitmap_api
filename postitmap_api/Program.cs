152d8b6d93d0c89e676e2c8879e35420739a46b7
oh <guruwang2@gmail.com>
2024-04-28 13:12:09 +0900

    웹소켓 서비스 생성

--- postitmap_api/Program.cs
+++ postitmap_api/Program.cs
@@ -1,3 +1,5 @@
+using postitmap_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(provider =>
+{
+    return new SocketService(System.Net.IPAddress.Any, 8080);
+});
 
 var app = builder.Build();
 
@@ -14,6 +20,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SocketService>();
 }
 
 app.UseHttpsRedirection();
---
+++ postitmap_api/Services/SocketService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using NetCoreServer;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace postitmap_api.Services
+{
+    public class SocketService : WsServer
+    {
+        public SocketService(IPAddress address, int port) : base(address, port)
+        {
+            Console.Write("Server starting...");
+            base.Start();
+        }
+        protected override TcpSession CreateSession() { return new ChatSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat WebSocket server caught an error with code {error}");
+        }
+
+
+    }
+
+    class ChatSession : WsSession
+    {
+        public ChatSession(WsServer server) : base(server) { }
+
+        public override void OnWsConnected(NetCoreServer.HttpRequest request)
+        {
+            Console.WriteLine($"Chat WebSocket session with Id {Id} connected!");
+
+            // Send invite message
+            string message = "Hello from WebSocket chat! Please send a message or '!' to disconnect the client!";
+            SendTextAsync(message);
+        }
+
+        public override void OnWsDisconnected()
+        {
+            Console.WriteLine($"Chat WebSocket session with Id {Id} disconnected!");
+        }
+
+        public override void OnWsReceived(byte[] buffer, long offset, long size)
+        {
+            string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
+            Console.WriteLine("Incoming: " + message);
+
+            // Multicast message to all connected sessions
+            ((WsServer)Server).MulticastText(message);
+
+            // If the buffer starts with '!' the disconnect the current session
+            if (message == "!")
+                Close();
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat WebSocket session caught an error with code {error}");
+        }
+    }
+}
